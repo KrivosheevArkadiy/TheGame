076ffaaca0e639e515ec939e7883d0982b6ac9f4
tietan <gryzikfafa@gmail.com>
2018-03-23 07:22:46 +0700

    new

---
+++ GroupGame/Assets/Scripts/Respawn.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Respawn : MonoBehaviour {
+
+    public GameObject _manager;
+    public int _class;
+    public GameObject _player;
+    
+
+    private void Awake()
+    {
+        _class = GameObject.Find("UIManager").GetComponent<UIManagerScript>()._pick;
+    }
+    void Start()
+    {
+        
+        _manager = GameObject.Find("UIManager");
+       
+
+        if (_class == 1)
+        {
+
+            
+            Instantiate(_player, transform.position, transform.rotation);
+            Debug.Log("war");
+        }
+    }
+
+
+}
---
+++ GroupGame/Assets/Scripts/UIManagerScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManagerScript : MonoBehaviour
+{
+    public GameObject _warrior;
+    public GameObject _manager;
+
+    
+    public int _pick = 0;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        
+    }
+
+    public void Start()
+    {
+        _warrior.GetComponent<Button>().onClick.AddListener(delegate { Pick(); });
+    }
+    public void Pick()
+    {
+        _pick = 1;
+        PlayerPrefs.SetFloat("pick",_pick);
+        Debug.Log("save warrior");
+        SceneManager.LoadScene("Level1");
+    }
+    public void OnApplicationQuit()
+    {
+        Application.Quit();
+    }
+
+    
+}
